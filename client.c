73efa340f0368b3663d2e0e8084f3a21a64ce096
dgski <gorski.dave@gmail.com>
2017-12-31 14:12:44 -0500

    Started working client to post transactions to nodes

---
+++ client.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+void display_help() {
+
+    printf("Help/Command List: 'h'\n");
+    printf("Transactions List: 't'\n");
+    printf("Post New Transaction: 'n sender reciever amount'\n");
+    printf("Quit program: 'q'\n");
+    return;
+}
+
+void quit_program() {
+    printf("Shutting down. Good bye!\n");
+    exit(0);
+
+}
+
+bool val_trans_format(char* sender, char* recipient, char* amount) {
+
+    for(int i = 0; i < strlen(sender); i++) {
+        if(sender[i] == ':') {
+            printf("You cannot use ':' in sender's or reciever's names.\n");
+            return false;
+        }
+    }
+    for(int i = 0; i < strlen(recipient); i++) {
+        if(recipient[i] == ':') {
+            printf("You cannot use ':' in senders or recipients names.\n");
+            return false;
+        }
+    }
+
+    printf("AMOUNT IN INT: %d\n", atoi(amount));
+
+    if(isnumber(atoi(amount))) {
+        printf("Enter a valid number amount to transfer.\n");
+        return false;
+    }
+
+    return true;
+}
+
+void post_transaction(char* input) {
+
+    char sender[32];
+    char recipient[32];
+    char amount[32];
+
+    sscanf(input, "%*s %s %s %s", sender, recipient, amount);
+
+    if(!val_trans_format(sender, recipient, amount))
+        return;
+
+    char out_msg[96];
+    char seperator[] = ":";
+    strcpy(out_msg, sender);
+    strcat(out_msg, seperator);
+    strcat(out_msg, recipient);
+    strcat(out_msg, seperator);
+    strcat(out_msg, amount);
+
+
+
+    return;
+
+}
+
+void print_trans() {
+
+    printf("TRANSACTIONS WILL BE HERE\n");
+
+    return;
+
+}
+
+
+
+int main(void) {
+
+    //Print heading and command block
+    printf("Blockchain in C: Client v0.1 by DG\n'h' for help/commandlist\n");
+
+    //Input buffer
+    char buffer[120] = {0};
+
+    while(true) {
+        printf("b-in-c>");
+        fgets(buffer, 120, stdin);
+        buffer[strlen(buffer) - 1] = 0;
+
+        if( !strcmp("h",buffer) || !strcmp("help",buffer) )
+            display_help();
+
+        else if( !strcmp("q", buffer) || !strcmp("quit", buffer) || !strcmp("exit", buffer) )
+            quit_program();
+
+        else if( !strcmp("t", buffer) )
+            print_trans();
+
+        else if(buffer[0] == 'n')
+            post_transaction(buffer);
+
+        else if( !strcmp("", buffer))
+            ;
+        else
+            printf("Command not recognized.\n");
+
+    }
+
+}
--- hashing.h
+++ hashing.h
@@ -6,7 +6,6 @@ void hash256(unsigned char* output, const char* input) {
     size_t length = strlen(input);
     unsigned char md[32];
     SHA256((const unsigned char*)input, length, md);
-
     memcpy(output,md, 32);
 
     //printf("sizeof result: %lu\n", sizeof(result));
